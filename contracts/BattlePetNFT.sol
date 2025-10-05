abaa1d7c9fe5975511a30c02cc6704c22a139249
Prinve59 <prinve59@gmail.com>
2025-10-05 08:58:59 +0530

    Save local changes before rebase

--- contracts/BattlePetNFT.sol
+++ contracts/BattlePetNFT.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BattlePetNFT is ERC721, Ownable {
-    uint256 private _nextTokenId;
+    uint256 public _nextTokenId; // made public to view next ID
 
     constructor() ERC721("BattlePet", "BPET") Ownable(msg.sender) {}
 
-    function mint(address to) public onlyOwner returns (uint256) {
+    // ✅ made public so anyone can mint (was onlyOwner before)
+    function mint(address to) public returns (uint256) {
         uint256 id = _nextTokenId++;
         _mint(to, id);
         return id;
     }
 
+    // already public
     function approveForMarket(address spender, uint256 tokenId) public {
         approve(spender, tokenId);
     }
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -18,7 +18,7 @@ contract Marketplace is Ownable {
         nft = IERC721(_nft);
     }
 
-    function listForSale(uint256 tokenId, uint256 price) external {
+    function listForSale(uint256 tokenId, uint256 price) public {
         require(nft.ownerOf(tokenId) == msg.sender, "Not owner");
         require(price > 0, "Price must be positive");
         require(listings[tokenId] == 0, "Already listed");
@@ -29,7 +29,7 @@ contract Marketplace is Ownable {
         emit Listed(tokenId, price, msg.sender);
     }
 
-    function buyItem(uint256 tokenId) external payable {
+    function buyItem(uint256 tokenId) public payable {
         uint256 price = listings[tokenId];
         require(price > 0, "Not listed");
         require(msg.value == price, "Incorrect payment");
@@ -44,7 +44,7 @@ contract Marketplace is Ownable {
         emit Bought(tokenId, msg.sender, price);
     }
 
-    function directSwap(uint256 tokenId1, uint256 tokenId2) external {
+    function directSwap(uint256 tokenId1, uint256 tokenId2) public {
         address owner1 = msg.sender;
         address owner2 = nft.ownerOf(tokenId2);
         require(nft.ownerOf(tokenId1) == owner1, "Not owner of token1");
@@ -57,8 +57,7 @@ contract Marketplace is Ownable {
         emit Swapped(tokenId1, tokenId2, owner1, owner2);
     }
 
-    // Optional: Owner can delist
-    function delist(uint256 tokenId) external {
+    function delist(uint256 tokenId) public {
         require(nft.ownerOf(tokenId) == address(this), "Not escrowed");
         require(tokenSellers[tokenId] == msg.sender, "Not seller");
 
